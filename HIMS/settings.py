71f08143fc7e01a84a6691cd0c9be3627f01c0d1
Jingyuan-L <Jingyuan-L@users.noreply.github.com>
2020-09-28 23:40:12 -0400

    create and connect to hims_oltp_db

--- HIMS/settings.py
+++ HIMS/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HIMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hims_oltp_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
