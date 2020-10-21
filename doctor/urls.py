6bfc3553c6d844563e849d17a278e7d1ce02691c
jiachenwang <jw5715@nyu.edu>
2020-10-22 01:33:22 +0800

    完成了病人的登录，注册，修改信息

--- doctor/urls.py
+++ doctor/urls.py
@@ -4,5 +4,8 @@ from .import views
 
 urlpatterns = [
     path('<int:pk>/', views.doctor_dashboard, name='doctor_dashboard'),
+    path('doc_view_appointment/<int:ap_id>/', views.doc_view_appointment, name='doc_view_appointment'),
+    path('treat/<int:ap_id>/', views.treat, name='treat'),
+    path('geticd/', views.geticd, name = 'geticd'),
 
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 import datetime
 
-from patient.models import Patient,PatAppointment,Treatment,Doctor,Hospital,Ins_Pat,InsuranceProvider,OutPatient
+from patient.models import Patient,PatAppointment,Treatment,Doctor,Hospital,Ins_Pat,InsuranceProvider,OutPatient,IcdTable
 
 
 # Create your views here.
@@ -22,3 +22,46 @@ def doctor_dashboard(request, pk):
         'today_ap':today_ap
     }
     return render(request, 'doctor/dashboard.html',context)
+
+
+@login_required(login_url='doctor_login')
+def doc_view_appointment(request, ap_id):
+    appointment = PatAppointment.objects.get(ap_id=ap_id)
+    doctor = appointment.doctor
+    treatment = Treatment.objects.filter(ap_id=ap_id)
+    inpatient, outpatient, nursinghome = None, None, None
+    if appointment.type == 'inpatient':
+        inpatient = InPatient.objects.get(ap_id=ap_id)
+    elif appointment.type == 'outpatient':
+        outpatient = OutPatient.objects.get(ap_id=ap_id)
+    elif appointment.type == 'nursinghome':
+        nursinghome = NursHmPatient.objects.get(ap_id=ap_id)
+    context = {
+        'doctor': doctor,
+        'appointment': appointment,
+        'treatment': treatment,
+        'inpatient': inpatient,
+        'outpatient': outpatient,
+        'nursinghome': nursinghome
+    }
+    return render(request, 'doctor/doc_view_appointment.html', context)
+
+@login_required(login_url='doctor_login')
+def treat(request, ap_id):
+    appointment = PatAppointment.objects.get(ap_id=ap_id)
+    doctor = appointment.doctor
+    context = {
+        'doctor': doctor,
+        'appointment': appointment,
+    }
+    return render(request, 'doctor/treat.html', context)
+
+@login_required(login_url='doctor_login')
+def geticd(request):
+    if request.method == 'GET':
+        icd = request.GET.get('icd')
+        print(1)
+        if icd:
+            data = list(IcdTable.objects.filter(icd_code__startswith=icd).values('icd_code','description'))
+            print(data[0])
+            return JsonResponse(data, safe=False)
