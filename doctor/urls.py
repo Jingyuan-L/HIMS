5477ec85647e487cb9465bece2dc8e6fe2bd518c
jiachenwang <jw5715@nyu.edu>
2020-10-22 11:52:46 +0800

    完成了病人的登录，注册，修改信息

--- doctor/urls.py
+++ doctor/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('doc_view_appointment/<int:ap_id>/', views.doc_view_appointment, name='doc_view_appointment'),
     path('treat/<int:ap_id>/', views.treat, name='treat'),
     path('geticd/', views.geticd, name = 'geticd'),
-
+    path('test/', views.test, name = 'test'),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -65,3 +65,7 @@ def geticd(request):
             data = list(IcdTable.objects.filter(icd_code__startswith=icd).values('icd_code','description'))
             print(data[0])
             return JsonResponse(data, safe=False)
+
+
+def test(request):
+    return render(request, 'doctor/text.html')
